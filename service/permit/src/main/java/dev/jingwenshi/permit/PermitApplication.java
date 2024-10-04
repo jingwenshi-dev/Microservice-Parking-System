d19aa088732ebc2ec051aef6101a5d93caff9468
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-10-03 22:52:52 -0400

    Initial commit

---
+++ service/permit/src/main/java/dev/jingwenshi/permit/PermitApplication.java
@@ -0,0 +1,13 @@
+package dev.jingwenshi.permit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermitApplication.class, args);
+	}
+
+}
---
+++ service/permit/src/test/java/dev/jingwenshi/permit/PermitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jingwenshi.permit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
