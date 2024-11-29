f9daa5a092b2059033e4b82d10af65d0c4065173
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-11-29 01:06:11 -0500

    Define TicketProcessor implements TicketManager

---
+++ service/violation/src/main/java/ca/mcmaster/cas735/acmepark/violation/business/TicketProcessor.java
@@ -0,0 +1,25 @@
+package ca.mcmaster.cas735.acmepark.violation.business;
+
+import ca.mcmaster.cas735.acmepark.violation.dto.TicketDTO;
+import ca.mcmaster.cas735.acmepark.violation.dto.TicketLookupDTO;
+import ca.mcmaster.cas735.acmepark.violation.port.provided.TicketManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketProcessor implements TicketManager {
+
+    @Override
+    public TicketDTO lookupTicket(TicketLookupDTO ticket) {
+        return null;
+    }
+
+    @Override
+    public String payTicket(String ticketId, String licensePlate) {
+        return "";
+    }
+
+    @Override
+    public String issueTicket(TicketDTO ticket) {
+        return "";
+    }
+}
