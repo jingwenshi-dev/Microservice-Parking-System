4c2c6e7d88abbd632692bcf446e0f348e4959967
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-11-30 17:43:00 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,23 @@
 
 
 cd ./service/gate || exit
-mvn clean package
+mvn clean package -DskipTests
 docker build -t gate .
 
 cd ../permit || exit
-mvn clean package
+mvn clean package -DskipTests
 docker build -t permit .
 
+cd ../service/payment || exit
+mvn clean package -DskipTests
+docker build -t payment .
+
+cd ../violation || exit
+mvn clean package -DskipTests
+docker build -t violation .
+
 cd ../visitor_access || exit
-mvn clean package
+mvn clean package -DskipTests
 docker build -t visitor_access .
 
 cd ../..
