b1c178dbe8d5487dd814b9def146dd73a867cfcb
FangZiyang <fangziyangryan@gmail.com>
2024-11-30 23:23:42 -0500

    add rabbitmq to payment

--- service/visitor_access/src/main/java/ca/mcmaster/cas735/acmepark/visitor_access/adapter/AMQPGateMessageListener.java
+++ service/visitor_access/src/main/java/ca/mcmaster/cas735/acmepark/visitor_access/adapter/AMQPGateMessageListener.java
@@ -26,7 +26,7 @@ public class AMQPGateMessageListener {
 
     // 监听 Gate 服务返回的进入请求的开门结果队列
     @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "gateEntryResponseQueue", durable = "ture"), // 定义队列名称为 gateEntryResponseQueue，并设置为持久化
-            exchange = @Exchange(value = "${app.custom.messaging.visitor-to-gate-entry-response-exchange}",
+            exchange = @Exchange(value = "${app.custom.messaging.visitor-entry-request-exchange}",
                     ignoreDeclarationExceptions = "true", type = "topic"), // 绑定到交换机，使用配置中的名称，类型为 topic
             key = "*")) // 路由键设置为 "*"，表示匹配任意路由键
     public void listenForGateEntryRequest(String data) {
