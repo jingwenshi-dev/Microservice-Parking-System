ca7b629bc73c6dd22c77c7629bfba2d0b6f8fa25
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-12-02 16:35:35 -0500

    Calculate total fines for violations.

--- service/payment/src/main/java/ca/mcmaster/cas735/acmepark/payment/business/PaymentService.java
+++ service/payment/src/main/java/ca/mcmaster/cas735/acmepark/payment/business/PaymentService.java
@@ -1,13 +1,13 @@
 package ca.mcmaster.cas735.acmepark.payment.business;
 
-import ca.mcmaster.cas735.acmepark.payment.business.entities.ParkingViolation;
+import ca.mcmaster.cas735.acmepark.payment.dto.TicketDTO;
+import ca.mcmaster.cas735.acmepark.payment.ports.ViolationInquirySender;
 import ca.mcmaster.cas735.acmepark.payment.ports.provided.PaymentCalculatorPort;
 import ca.mcmaster.cas735.acmepark.payment.dto.PaymentRequest;
 import ca.mcmaster.cas735.acmepark.payment.factory.PaymentCalculatorFactory;
 import ca.mcmaster.cas735.acmepark.payment.factory.PaymentStrategyFactory;
 import ca.mcmaster.cas735.acmepark.payment.ports.provided.PaymentStrategy;
 import ca.mcmaster.cas735.acmepark.payment.ports.provided.PaymentServicePort;
-import ca.mcmaster.cas735.acmepark.payment.ports.required.ParkingViolationsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,38 +24,34 @@ public class PaymentService implements PaymentServicePort {
 
     private final PaymentStrategyFactory paymentStrategyFactory;
     private final PaymentCalculatorFactory paymentCalculatorFactory;
-    private final ParkingViolationsRepository violationsRepository;
+    private final ViolationInquirySender violationInquirySender;
 
     @Autowired
-    public PaymentService(PaymentStrategyFactory paymentStrategyFactory,
-                          PaymentCalculatorFactory paymentCalculatorFactory,
-                          ParkingViolationsRepository violationsRepository) {
+    public PaymentService(PaymentStrategyFactory paymentStrategyFactory, PaymentCalculatorFactory paymentCalculatorFactory, ViolationInquirySender violationInquirySender) {
         this.paymentStrategyFactory = paymentStrategyFactory;
         this.paymentCalculatorFactory = paymentCalculatorFactory;
-        this.violationsRepository = violationsRepository;
+        this.violationInquirySender = violationInquirySender;
     }
 
     @Override
     public boolean processPayment(PaymentRequest paymentRequest) {
         try {
-            // TODO:确认是否需要port and adapter
             // 根据用户类型选择支付计算器
             PaymentCalculatorPort paymentCalculator = paymentCalculatorFactory.getPaymentCalculator(paymentRequest.getUserType());
 
             // 计算停车费用
             BigDecimal amount = paymentCalculator.calculateParkingFee(paymentRequest);
 
-            // 查询未支付的罚单金额
-            List<ParkingViolation> unpaidViolations = violationsRepository.findByLicensePlateAndIsPaidFalse(paymentRequest.getLicensePlate());
-            BigDecimal totalFines = unpaidViolations.stream()
-                    .map(ParkingViolation::getFineAmount)
-                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            // Send request for violation inquiry and calculate total fines
+            List<TicketDTO> violations = violationInquirySender.sendInquiry(paymentRequest.getLicensePlate());
+            BigDecimal totalFines = BigDecimal.ZERO;
+            for (TicketDTO violation : violations) {
+                totalFines = totalFines.add(violation.getFineAmount());
+            }
 
             // 计算总费用
             BigDecimal totalAmount = paymentRequest.getAmount().add(totalFines);
             paymentRequest.setAmount(totalAmount);
-            // TODO：支付成功，删除之前的罚款
-            // TODO: 测试需要单独创建一个吗，还是直接在项目里面，老师怎么验证所有的逻辑
 
             // 根据支付方式选择支付策略
             PaymentStrategy paymentStrategy = paymentStrategyFactory.getPaymentStrategy(paymentRequest.getPaymentMethod());
