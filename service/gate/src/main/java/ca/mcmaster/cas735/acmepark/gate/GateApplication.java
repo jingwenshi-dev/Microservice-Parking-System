24676ccfe565e902b5578934c1e0fd26903b54c9
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-12-03 16:22:33 -0500

    Update Gate configuration files for Eureka service

--- service/gate/src/main/java/ca/mcmaster/cas735/acmepark/gate/GateApplication.java
+++ service/gate/src/main/java/ca/mcmaster/cas735/acmepark/gate/GateApplication.java
@@ -2,8 +2,10 @@ package ca.mcmaster.cas735.acmepark.gate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GateApplication {
 
 	public static void main(String[] args) {
