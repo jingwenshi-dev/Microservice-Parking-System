828e7801ec91a74882f26d97ed7f585eb79a74bd
yaqi zhang <regana.zhang@mail.utoronto.ca>
2024-12-03 15:20:17 -0500

    open port for check permit sales

--- service/permit/src/main/java/ca/mcmaster/cas735/acmepark/permit/adapter/PermitController.java
+++ service/permit/src/main/java/ca/mcmaster/cas735/acmepark/permit/adapter/PermitController.java
@@ -7,10 +7,7 @@ import ca.mcmaster.cas735.acmepark.permit.port.PermitApplicationPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/permits")
@@ -52,4 +49,16 @@ public class PermitController {
             return new ResponseEntity<>("Failed to initiate permit renewal application: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/valid-permits")
+    public ResponseEntity<Integer> getValidPermitCount() {
+        try {
+            int validPermitCount = permitApplicationPort.getValidPermitCount();
+            return new ResponseEntity<>(validPermitCount, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(0, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
