16601c1c994ed8113cddd27ef954ac6718c1907b
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-12-04 19:21:27 -0500

    Visitor service code cleanup

--- service/payment/src/main/java/ca/mcmaster/cas735/acmepark/payment/dto/PaymentRequest.java
+++ service/payment/src/main/java/ca/mcmaster/cas735/acmepark/payment/dto/PaymentRequest.java
@@ -1,12 +1,10 @@
 package ca.mcmaster.cas735.acmepark.payment.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
